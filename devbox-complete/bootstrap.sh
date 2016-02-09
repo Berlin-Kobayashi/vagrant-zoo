166c25d50e20614e075b7f79e279af5567461f1d
DanShu <d.schulz93@gmx.de>
2016-02-09 19:10:44 +0100

    - changed in devbox-complete    - enabled .htaccess for apache2    - opened MongoDB port for everyone

--- devbox-complete/bootstrap.sh
+++ devbox-complete/bootstrap.sh
@@ -5,12 +5,16 @@ sed -i 's/^.*force_color_prompt=yes.*/force_color_prompt=yes/' .bashrc
 source .bashrc
 
 # install apache2
-apt-get update
-apt-get install -y apache2
+sudo apt-get update
+sudo apt-get install -y apache2
 if ! [ -L /var/www ]; then
   rm -rf /var/www/html
   ln -fs /vagrant /var/www/html
 fi
+
+# enable apache2 rewrite module
+sudo a2enmod rewrite
+sudo service apache2 restart
 
 # install php 5.6
 sudo add-apt-repository -y ppa:ondrej/php5-5.6
@@ -20,6 +24,13 @@ sudo apt-get install -y php5
 
 # install Git
 sudo apt-get install -y git
+
+# install common command line tools
+git clone https://github.com/DanShu93/common-command-line-tools.git /home/vagrant/common-command-line-tools
+
+# enable .htaccess
+sudo php /home/vagrant/common-command-line-tools/apache2/htaccessEnabler.php
+sudo service apache2 restart
 
 # install composer
 curl -sS https://getcomposer.org/installer | sudo -H php -- --install-dir=/usr/local/bin --filename=composer
@@ -38,6 +49,7 @@ sudo apt-get -y install mysql-server
 
 # install MYSQL PHP extension
 sudo apt-get install php5-mysql
+sudo service apache2 restart
 
 # install MongoDB
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
@@ -46,18 +58,24 @@ echo "deb http://repo.mongodb.org/apt/ubuntu trusty/mongodb-org/3.2 multiverse" 
 sudo apt-get update
 sudo apt-get install -y mongodb-org
 
+# open MongoDB port for everyone
+sudo iptables -A INPUT -p tcp --dport 27017 -j ACCEPT
+sudo sed -i 's/^  bindIp: 127\.0\.0\.1$/#  bindIp: 127\.0\.0\.1/' /etc/mongod.conf
+sudo service mongod restart
+
 # install MongoDB PHP extension
 sudo apt-get install php5-mongo
+sudo service apache2 restart
 
 # install golang
 curl -O https://storage.googleapis.com/golang/go1.5.3.linux-amd64.tar.gz
 tar -xvf go1.5.3.linux-amd64.tar.gz
 sudo mv go /usr/local
 rm go1.5.3.linux-amd64.tar.gz
-echo 'export PATH=$PATH:/usr/local/go/bin' >> .bashrc
+echo 'export PATH=$PATH:/usr/local/go/bin' >> /home/vagrant/.bashrc
 mkdir goground
-echo 'export GOPATH=$HOME/goground' >> .bashrc
-source .bashrc
+echo 'export GOPATH=$HOME/goground' >> /home/vagrant/.bashrc
+source /home/vagrant/.bashrc
 
 # install Java 8 JDK
 sudo add-apt-repository -y ppa:webupd8team/java
